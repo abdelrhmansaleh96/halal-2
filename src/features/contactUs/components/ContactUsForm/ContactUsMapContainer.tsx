676b8799bcb208d500659efe538398a60917bd8b
abdelrhman saleh <81262213+abdelrhmansaleh96@users.noreply.github.com>
2024-05-24 15:07:54 +0300

    contact-us--map

---
+++ src/features/contactUs/components/ContactUsForm/ContactUsMapContainer.tsx
@@ -0,0 +1,26 @@
+import { ContactUsMap } from "./ContactUsMap";
+import { CustomBreadcrumbs } from "@/shared/components/customBreadcrumbs/customBreadcrumbs";
+import { MainContainer } from "@/shared/components/main-container/MainContainer";
+import { useTranslation } from "react-i18next";
+
+export const ContactUsMapContainer: React.FC = () => {
+  const { t } = useTranslation();
+  const breadcrumbs = [
+    {
+      name: t("links.home"),
+      path: "/",
+    },
+    {
+      name: t("links.contactUs"),
+      path: "/contactUs",
+    },
+  ];
+  return (
+    <div className="w-full h-[calc(100vh-120px)] pt-4 bg-muted">
+      <MainContainer>
+        <CustomBreadcrumbs breadcrumbs={breadcrumbs} />
+        <ContactUsMap />
+      </MainContainer>
+    </div>
+  );
+};
